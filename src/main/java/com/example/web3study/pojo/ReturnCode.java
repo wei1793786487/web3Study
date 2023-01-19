9eb23589bb92686282a500ada6b18a305043c84f
sun <1793786487@qq.com>
2023-01-19 21:36:45 +0800

    1 分离区块链操作的日志 2 修复提取错误信息的不同链的处理方法

--- src/main/java/com/example/web3study/pojo/ReturnCode.java
+++ src/main/java/com/example/web3study/pojo/ReturnCode.java
@@ -10,9 +10,10 @@ public enum ReturnCode {
     NFT_NAME_NULL(1001,"nft的名称不能为空"),
     IPFS_UPLOAD_ERROR(1002,"ipfs文件上传异常"),
 
+
     NFT_MINT_ERROR(1003,"nft铸造异常"),
 
-
+    PARAMETER_ERROR(1004,"参数异常"),
 
     INVALID_TOKEN(2001,"访问令牌不合法");
 
--- src/main/java/com/example/web3study/utils/Web3Utils.java
+++ src/main/java/com/example/web3study/utils/Web3Utils.java
@@ -1,19 +1,25 @@
 package com.example.web3study.utils;
 
-import com.example.web3study.pojo.Web3Account;
-import com.example.web3study.pojo.Web3TransactionError;
+import com.example.web3study.exception.XxException;
+import com.example.web3study.pojo.*;
+import com.example.web3study.service.BlockchainLogService;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.web3j.crypto.*;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tx.Contract;
 import org.web3j.utils.Numeric;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.web3j.crypto.Hash.sha256;
@@ -24,6 +30,27 @@ import static org.web3j.crypto.Hash.sha256;
 public class Web3Utils {
 
 
+    private  static Credentials credentials;
+
+
+    private static String blockchainName;
+
+    private static BlockchainLogService blockchainLogService;
+
+    @Resource
+    public void setCredentials(Credentials credentials) {
+        Web3Utils.credentials = credentials;
+    }
+
+    @Resource
+    public  void setBlockchainLogService(BlockchainLogService blockchainLogService) {
+        Web3Utils.blockchainLogService = blockchainLogService;
+    }
+
+    @Value("${web3j.blockchain_name}")
+    public void setBlockchainName(String blockchainName) {
+        Web3Utils.blockchainName = blockchainName;
+    }
 
     public static Web3Account getWalletByWords(String word){
         byte[] seed = MnemonicUtils.generateSeed(word, "");
@@ -50,16 +77,62 @@ public class Web3Utils {
     }
 
     public static Web3TransactionError web3jErrorToPojo(String errorMessage){
+        System.out.println(errorMessage);
+        Gson gson = new Gson();
+        //todo 这里要做特殊处理 对待不同的区块链 如果为了简便可以直接返回errorMessage
         try {
-            Gson gson = new Gson();
-            Map map = gson.fromJson(errorMessage, Map.class);
-            String hash = (String) map.keySet().toArray()[0];
-            Map map1 = (Map) map.get(hash);
-            return new Web3TransactionError(hash, (String) map1.get("error"), (Double) map1.get("program_counter"), (String) map1.get("return"), (String) map1.get("reason"));
-        } catch (JsonSyntaxException e) {
-            //有的链不返回错误码 返回的不是json数据 所以做一个报错处理
-            return  new Web3TransactionError(errorMessage);
+            if ("测试私链".equals(blockchainName)){
+    //  返回格式如下  {"hash":"0x5803ee1511c5ce7da602e3a90ac55dcc7f137b903cd00e01bce07e4bce4994a5","programCounter":0,"result":"0x5803ee1511c5ce7da602e3a90ac55dcc7f137b903cd00e01bce07e4bce4994a5","reason":null,"message":"Transaction's maxFeePerGas (1) is less than the block's baseFeePerGas (737164) (vm hf=merge -> block -> tx)"}
+                HashMap<String,Object> map = gson.fromJson(errorMessage, HashMap.class);
+                return new Web3TransactionError((String) map.get("hash"),(String)map.get("message"),(Double)map.get("programCounter"),(String)map.get("result"),(String)map.get("reason"));
+            }
+            if ("Bsn武汉链".equals(blockchainName)){
+                Map map = gson.fromJson(errorMessage, Map.class);
+                String hash = (String) map.keySet().toArray()[0];
+                Map map1 = (Map) map.get(hash);
+                return new Web3TransactionError(hash, (String) map1.get("error"), (Double) map1.get("program_counter"), (String) map1.get("return"), (String) map1.get("reason"));
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new Web3TransactionError(errorMessage);
+        }
+        return new Web3TransactionError(errorMessage);
+    }
+
+    public static Integer generateContractInformation ( Contract contract,Integer status){
+        return generateContractInformation(contract,status,null);
+    }
+    /**
+     *
+     * @param contract
+     * @param status  0是错误的 1是成功的
+     * @return 插入的区块链日志的id
+     */
+    public static Integer generateContractInformation ( Contract contract,Integer status,String error){
+        try {
+            if(status!=1&&status!=0){
+                throw new XxException(ReturnCode.PARAMETER_ERROR);
+            }
+            BlockchainLog blockchainLog = new BlockchainLog();
+            blockchainLog.setMintAddress(credentials.getAddress());
+            blockchainLog.setBlockchain(blockchainName);
+            if (status==1){
+                TransactionReceipt transactionReceipt = contract.getTransactionReceipt().get();
+                blockchainLog.setContractAddress(contract.getContractAddress());
+                blockchainLog.setState(1);
+                blockchainLog.setGasSpend(transactionReceipt.getGasUsed().longValue());
+            }else {
+                blockchainLog.setErrorInfo(error);
+                blockchainLog.setState(0);
+            }
+            int id = blockchainLogService.insert(blockchainLog);
+            return id;
+        } catch (Exception e) {
+            log.error(e.toString());
+            throw new RuntimeException(e);
         }
     }
+
+
 
 }
